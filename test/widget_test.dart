ec73f31b0cee124644175d292e9a613e4076833c
Armando Ruiz <aranruiz13@gmail.com>
2021-01-24 10:10:03 -0600

    Add test for CounterApp widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/counter/counter.dart';
+import 'package:flutter_bloc_app/counter/counter_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_bloc_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group('CounterApp', () {
+    testWidgets('is a MaterialApp', (tester) async {
+      expect(CounterApp(), isA<MaterialApp>());
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    testWidgets('home is CounterPage', (tester) async {
+      expect(CounterPage(), isA<CounterPage>());
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(CounterApp());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
   });
 }
