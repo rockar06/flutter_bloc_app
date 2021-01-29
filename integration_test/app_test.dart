6c583c0734aeabb877f0c92adbad9f20d454abcf
Armando Ruiz <aranruiz13@gmail.com>
2021-01-29 15:48:26 -0600

    Add integration test to Counter app

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,20 +1,19 @@
-// This is a basic Flutter integration test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:flutter_bloc_app/main.dart' as app;
+const _incrementButtonKey = Key('counterView_increment_floatingActionButton');
+const _decrementButtonKey = Key('counterView_decrement_floatingActionButton');
 
-void main() => run(_testMain);
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  _testMain();
+}
 
 void _testMain() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     app.main();
 
@@ -26,11 +25,26 @@ void _testMain() {
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byKey(_incrementButtonKey));
     await tester.pump();
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Counter decrement smoke test', (tester) async {
+    // Build our app and trigger a frame.
+    app.main();
+    await tester.pumpAndSettle();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byKey(_decrementButtonKey));
+    await tester.pump();
+
+    expect(find.text('-1'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
   });
 }
